82d6da2416f320b2d7de3d12801edf9257df1ca8
Cahigan-MarkLouie <markcahigan54@gmail.com>
2024-09-22 18:56:13 +0800

    add jSeries object and call print function

--- script.ts
+++ script.ts
@@ -13,3 +13,17 @@ type Phone = {
   color: string;
   print(): void;
 }
+
+const jSeries: Phone = {
+  brand: "Samsung",
+  processor: Processor.EXYNOS,
+  memory: "16GB",
+  color: "Black",
+  print(){
+    console.log("My " + this.brand + " phone is colored " + this.color + 
+        " that has a RAM of " + this.memory + 
+        " with a processor called " + this.processor);
+  }
+};
+
+jSeries.print();
