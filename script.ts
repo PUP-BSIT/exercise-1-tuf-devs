949b269b0ba839c3170bd00eab0875de48bc0755
Jheferson Añonuevo <jhefersonzanonuevo@gmail.com>
2024-09-22 18:51:56 +0800

    add redmi object and call print method

--- script.ts
+++ script.ts
@@ -13,3 +13,20 @@ type Phone = {
   color: string;
   print(): void;
 }
+
+const redmi: Phone = {
+  brand: "Xiaomi",
+  processor: Processor.SNAPDRAGON,
+  memory: "4Gb",
+  color: "Titanium Black",
+  print: function(): void {
+    console.log("The Brand of my Phone is " + this.brand + " and my Processor"
+        + " is " + this.processor + ".\n" + 
+        "Which is one of the famous phone" + " processor in the market.\n" + 
+        "It also has a " + this.memory + 
+        " Ram for Multitasking.\n" + "My Phone has a color of " + 
+        this.color + " which looks very premium.");
+  }
+};
+
+redmi.print();
